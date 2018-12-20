f0dcea9183ea2039e006baa8e0f1421f83415362
vktsn <44133566+vktsn@users.noreply.github.com>
2018-12-21 02:32:56 +0300

    Update api.py

--- homework04/api.py
+++ homework04/api.py
@@ -1,10 +1,8 @@
 import requests
 import time
-
 import config
 
-# выполняет гет-запрос по адресу, при необходимости повторяет
-# запрос указанное кол-во раз по алгоритму экспоненциальной задержки
+
 def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
     """ Выполнить GET-запрос
     :param url: адрес, на который необходимо выполнить запрос
@@ -22,7 +20,6 @@ def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
         # если не удалось получить ответ
         # обработчик ошибки
         except requests.exceptions.RequestException:
-            # если и максимально
             if i == max_retries - 1:
                 raise
             # время задержки равно коэф * 2 * и
@@ -51,67 +48,60 @@ def get_friends(user_id, fields):
         'fields': fields,
         'v': config.VK_CONFIG['version']
     }
-    # получение адреса для запроса
-    query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v={v}".format(**query_params)
-    #запрос
-    response = get(query)
-    #получение ответа в формате джейсон
-    return response.json()
+    query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v={v}".format(
+        **query_params)
+    # запрос по указанному адресу с заданными параметрами
+    response = get(query, query_params)
+    # переводим в джейсон
+    json_file = response.json()
+    fail = json_file.get('error')
+    if fail:
+        raise Exception(json_file['error']['error_msg'])
+    return json_file['response']['items']
 
 
-# получение переписки с указанным пользователем
-def messages_get_history(user_id, offset=0, count=200) -> list:
+
+
+
+def messages_get_history(user_id: int, offset=0, count=200) -> list:
     """ Получить историю переписки с указанным пользователем
     :param user_id: идентификатор пользователя, с которым нужно получить историю переписки
     :param offset: смещение в истории переписки
     :param count: число сообщений, которое нужно получить
     """
-
-    # айди, смещение и число сообщений должны быть положительными инт
+    # айди, статус и счетчик должны быть положительными инт
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
     assert isinstance(offset, int), "offset must be positive integer"
     assert offset >= 0, "user_id must be positive integer"
     assert count >= 0, "user_id must be positive integer"
-    # параметры запроса
     query_params = {
         'domain': config.VK_CONFIG['domain'],
         'access_token': config.VK_CONFIG['access_token'],
         'user_id': user_id,
         'offset': offset,
-        'count': count,
+        'messages_count': min(count, 200),
         'v': config.VK_CONFIG['version']
     }
 
-    # сообщения - пустой список
+    # обнуляем список сообщений
     messages = []
-    try:
-        # адрес для запроса
-        query = "{domain}/messages.getHistory?access_token={access_token}&user_id={user_id}&v={v}".format(
-            **query_params)
-        # выполнение запроса
-        response = get(query)
-        # получение результата запроса в формате джейсон
-        json_doc = response.json()
-        fail = json_doc.get('error')
-        if fail:
-            raise Exception(json_doc['error']['error_msg'])
-        count = json_doc['response']['count']
-        # пока счетчик больше 0
-        while count > 0:
-            # адрес для запроса, его выполнение, получение результата в формате джейсон
-            query = "{domain}/messages.getHistory?access_token={access_token}&user_id={user_id}&offset={offset}&count={count}&v={v}".format(
-                **query_params)
-            response = get(query)
-            json_doc = response.json()
-            fail = json_doc.get('error')
-            if fail:
-                raise Exception(json_doc['error']['error_msg'])
-            messages.extend(json_doc['response']["items"])
-            count -= min(count, 200)
-            query_params['offset'] += 200
-            query_params['count'] = min(count, 200)
-            time.sleep(0.4)
-    finally:
-        return messages
-    
+    while count > 0:
+        query = "{domain}/messages.getHistory?" \
+            "access_token={access_token}&user_id={user_id}&offset={offset}&count={messages_count}&v={v}"\
+            .format(**query_params)
+        # выполняем запрос по нужному адресу с нужными параметрами
+        response = get(query, query_params)
+        # если есть результат
+        if response:
+            # переводим в джейсон, проверяем, чтобы там не было ошибки
+            json_file = response.json()
+            if json_file.get('error') is not None:
+                print(json_file['error']['error_msg'])
+            else:
+                messages.extend(json_file['response']["items"])
+        count -= min(count, 200)
+        query_params['offset'] += 200
+        query_params['messages_count'] = min(count, 200)
+        time.sleep(0.4)
+    return messages
