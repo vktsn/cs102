9e42b27e348aba53a2ff31291f3f4bc80ad2a45a
vktsn <44133566+vktsn@users.noreply.github.com>
2018-12-21 02:11:34 +0300

    Update age.py

--- homework04/age.py
+++ homework04/age.py
@@ -1,9 +1,8 @@
-import datetime
+from datetime import datetime
 from statistics import median
 from typing import Optional
-
 from api import get_friends
-from api_models import User
+import config
 
 
 def age_predict(user_id: int) -> Optional[float]:
@@ -12,45 +11,54 @@ def age_predict(user_id: int) -> Optional[float]:
     :param user_id: идентификатор пользователя
     :return: медианный возраст пользователя
     """
-
-    # айди должен быть положительным инт
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
 
-    # получаем список друзей и их даты рождения
-    friends = get_friends(user_id, "bdate")
-
-    friends = [User(**friend) for friend in friends]
-    # список возрастов
+    # обнуляем списки, в которых будут даты рождения и возрасты
+    dates = []
     ages = []
+    # получаем даты рождения
+    data = get_friends(user_id, 'bdate')
+    # для каждого значение дата
+    for i in data:
+        # если дата рождения указана
+        if i.get('bdate'):
+            # добавляем в список дат новую
+            dates.append(i['bdate'])
 
-    for friend in friends:
-        # если у пользователя указана дата рождения
-        if friend.bdate is not None:
-            # получаем дату рождения, разделенную точками
-            data = friend.bdate.split(".")
-            # если у даты рождения три параметра
-            if len(data) == 3:
-                # год рождения - последнее значение
-                byear = int(data[2])
-                # месяц рождения - второе значение
-                bmonth = int(data[1])
-                # день рождения - первое знаничение
-                bday = int(data[0])
-                # если месяц сейчас меньше месяца рождения
-                # или месяцы равны, но день сейчас меньше дня рождения
-                if (datetime.now().month < bmonth) or (datetime.now().month == bmonth and datetime.now().day < bday):
-                    # возраст уменьшить на год
-                    age = datetime.now().year - byear - 1
-                else:
-                    # иначе возраст равен год сейчас - год рождения
-                    age = datetime.now().year - byear
-                # добавляем полученное знание возраста в список возрастов
-                ages.append(age)
-    # если длина списка больше 0
+    # обнуляем список, в котором будут даты с годом
+    y_dates = []
+    for elem in dates:
+        # такая длина будет только у тех дат, где указан год
+        # учитываются точки, у однозначных чисел нет нуля
+        if len(elem) in range(8, 11):
+            # добавляем в список новую дату
+            y_dates.append(elem)
+    # заменяем список со старыми датами на те, где указан год
+    dates = y_dates
+
+    # для элементов в списке
+    for elem in dates:
+        # мап - нужно применить функцию к каждому элементу списка
+        # т.е. все элементы списка перевели в инт
+        a = list(map(int, elem.split('.')))
+        # где а(2) - год, а(1) - месяц, а(0) - день рождения
+        age = datetime.now().year - a[2]
+        # если у человека еще не было др в этом году, возраст - 1
+        if (datetime.now().month < a[1]) or (
+                datetime.now().month == a[1] and datetime.now().day < a[0]):
+            ages.append(age - 1)
+        else:
+            ages.append(age)
+
+    # если нашлись возрасты вернуть среднее значение
     if len(ages) > 0:
-        # вернуть среднее значение элементов из списка
         return median(ages)
     else:
-        # иначе не возвращать ничего
         return None
+
+
+if __name__ == '__main__':
+    user_id = int(config.VK_CONFIG['user_id'])
+    print('Age:', int(age_predict(user_id)))
+    
