0263635776064fa54c694248d565c184b331217a
vktsn <44133566+vktsn@users.noreply.github.com>
2019-03-31 21:28:42 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer 
+from sqlalchemy import Column, String, Integer
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
